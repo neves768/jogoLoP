19abaac0460610a66e52fd1e587cb9da3ecc438e
Christopher Neves <chris_oboy@hotmail.com>
2018-11-10 00:37:59 -0200

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ function setup() {
 	createCanvas(400, 400);
 }
 
-var x = 25,
-	move = 0;
+var x = 25,	move = 0;
 
 function keyPressed() {
 	if (keyCode === 65 || keyCode === 37) {
@@ -24,15 +23,15 @@ function draw() {
 	if (move != 0) {
 		if (move === -1) {
 			x -= 0.5;
-		}	else if (move===1) {
+		} else if (move===1) {
 			x+= 0.5;
 		}
 	}
-		ellipseMode(CORNER);
-		fill(2);
-		ellipse(x, 25, 50, 50);
-		ellipseMode(CENTER);
-		fill(255, 0, 0);
-		ellipse(x + 23, 20, 25, 25);
+	ellipseMode(CORNER);
+	fill(2);
+	ellipse(x, 25, 50, 50);
+	ellipseMode(CENTER);
+	fill(255, 0, 0);
+	ellipse(x + 23, 20, 25, 25);
 	quad(300, 20, 340, 20, 340, 60, 300, 60);
-	}
+}
