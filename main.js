d80b9f87d8e6828140d3adf3987e43ef6c40294d
Christopher Neves <chris_oboy@hotmail.com>
2018-11-17 18:12:08 -0300

    Rotação solucionada
    Cálculos não foram necessários.
    PS: Teria mais elegância e valor com os cálculos que fizemos, mas foram todos em vão. Altas horas de estudo e cálculo para ter uma simples solução.

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 var img, x = 200,
   y = 200,
   rot = 0,
-  spd = 1;
-var bico_x = 216,
-  bico_y = 200;
+  spd = 0,
+  canvas;
 var t = {};
 
 function setup() {
@@ -15,48 +14,44 @@ function setup() {
 function move() {
   if (keyIsDown(68) || keyIsDown(39)) { // D
     if (rot >= 360) rot = 0;
-    rot += 1;
+    rot += 0.5;
   }
   if (keyIsDown(65) || keyIsDown(37)) { // A
     if (rot <= 0) rot = 360;
-    rot -= 1;
+    rot -= 0.5;
   }
   if (keyIsDown(87) || keyIsDown(38)) { // W
-    if (rot >= 0 && rot <= 90) { // único funcional até agora
-      // rot = 90 => x=0; y+=1
-      // rot = 0 => x+=1; y=0
-      y += (spd/90) * rot;
-      x += spd - (rot / 90); // rot90.x = 0; rot0.x = 1;
-    } else if (rot >= 270 && rot <= 360) {
-      // rot = 270 => x=0; y-=1;
-      // rot = 360 => x+=1; y=0;
-      newRot = 360-rot;
-      console.log(rot);
-      x += 1 - (rot / 270);
-      y -= 0.0037037037037 * rot;
-    } else if (rot > 90 && rot <= 180) {
-      // rot = 180 => x-=1; y=0;
-      // rot = 90 => x=0; y+=1;
-    } else if (rot > 180 && rot < 270) {}
-
+    if (spd < 1) spd += 0.01;
   }
   if (keyIsDown(83) || keyIsDown(40)) { // S
-    y += spd;
+    if (spd > -1) spd -= 0.01;
   }
+}
+
+// Interface
+function dInterface(){
+  textSize(20);
+	text('Velocidade: '+Math.floor(spd*10)+' ', 10, 30);
+  text('Rotação: '+Math.floor(rot)+' ', 150, 30);
 }
 
 function draw() {
   background('#37a2e7');
+  // Interface
+  	dInterface();
+  // Elementos do mapa
+  fill(12);
+  quad(555, 300, 500, 400, 500, 100, 400, 100);
+  
+  // Personagem
+  translate(x, y);
+  //vmk = atan2(mouseY - y, mouseX - x);
+  rotate(radians(rot));
+  imageMode(CENTER);
+  image(img, 0, 0, 100, 100);
+
   // Movimentação
   move();
-
-  // Personagem
-  quad(x + 100, y, 220, 220, 200, 300, 300, 220);
-  translate(x, y);
-  vmk = atan2(mouseY - y, mouseX - x);
-  angleMode(DEGREES);
-  rotate(rot);
-  imageMode(CENTER);
-  image(img, 0, 0, 200, 200);
-  push();
+  y += sin(radians(rot)) * spd;
+  x += cos(radians(rot)) * spd;
 }
